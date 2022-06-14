38c717f63d609594647234cf010edffb7bc17a43
TejasJakkawar <105486556+TejasJakkawar@users.noreply.github.com>
2022-06-14 15:40:27 +0530

    Create Resume.js

---
+++ Resume.js
@@ -0,0 +1,46 @@
+ var resume={
+    "Name": "Tejas Jakkawar",
+    "Mobile": "8983141487",
+    "Age": "23",
+    "Email": "jakkawartejas@gmail.com",
+    "Address": "Chintamani layout 1, pandharkawada, 445302",
+    "Education": [
+        {
+            "type": "SSC",
+            "SChool Name": "Dr Yardy English High School",
+            "Passing year": "2014",
+            "Percentage": "91.80"
+        },
+        {
+            "type": "HSC",
+            "School Name": "Smt nankibai Wadhwani junior College, Yavatmal",
+            "Passing year": "2016",
+            "Percentage": "66.30"
+        },
+        {
+            "type": "UG",
+            "School Name": "D Y Patil institute of engineering management and research, Pune",
+            "Passing year": "2020",
+            "Percentage": "68.72"
+        }
+    ],
+    "Experience": [
+        {
+            "Company name": "Indovance Inc",
+            "Duration": "Sept 2020 to Dec 2020",
+            "Ddesignation": "Civil Engineer",
+        },
+        {
+            "Company name": "Efficiently Global Solutions pvt ltd",
+            "Duration": "Oct 2021 to Present",
+            "Ddesignation": "Civil Developent Engineer",
+        }
+    ],
+    "Software Knowledge": {
+        "Autocad": "Intermediate",
+        "Civil 3D": "Intermediate",
+        "Bluebeam": "Advanced",
+    },
+    "Hobbies": ["Travelling, Trekking, Listening music"],
+    "Languages": ["English, Marathi, Hindi "],
+}
